95fa882cfe3b88e7eb24373337c88a6b8636fc16
Hardikothiya <hardikothiya@gmail.com>
2021-11-10 21:47:53 +0530

    completed

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -4,21 +4,29 @@ class MyButton extends StatelessWidget {
   final color;
   final textcolor;
   final String buttonText;
+  final buttonTapped;
 
-  MyButton({this.color, this.textcolor, required this.buttonText});
+  MyButton(
+      {this.color,
+      this.textcolor,
+      required this.buttonText,
+      this.buttonTapped});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: Container(
-          color: color,
-          child: Center(
-            child: Text(
-              buttonText,
-              style: TextStyle(color: textcolor),
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(color: textcolor, fontSize: 20.0),
+              ),
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_demo/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -41,14 +43,39 @@ class _HomePageState extends State<HomePage> {
     '='
   ];
 
+  var userQuestion = '';
+  var userAnswer = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple.shade100,
+      backgroundColor: Color(0xFFCBBCE7),
       body: Column(
         children: [
           Expanded(
-            child: Container(),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(height: 50.0),
+                  Container(
+                      padding: EdgeInsets.all(20.0),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        userQuestion,
+                        style: TextStyle(fontSize: 20.0),
+                      )),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      userAnswer,
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
           Expanded(
             flex: 2,
@@ -57,20 +84,52 @@ class _HomePageState extends State<HomePage> {
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 4),
                 itemBuilder: (BuildContext context, int index) {
+                  //clear
                   if (index == 0) {
                     return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = '';
+                          userAnswer = '';
+                        });
+                      },
                       buttonText: buttons[index],
                       color: Colors.green,
                       textcolor: Colors.white,
                     );
+                    //delete
                   } else if (index == 1) {
                     return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = userQuestion.substring(
+                              0, userQuestion.length - 1);
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.red,
+                      textcolor: Colors.white,
+                    );
+                  }
+                  //equal
+                  else if (index == buttons.length - 1) {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          equalPressed();
+                        });
+                      },
                       buttonText: buttons[index],
                       color: Colors.red,
                       textcolor: Colors.white,
                     );
                   } else {
                     return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion += buttons[index];
+                        });
+                      },
                       buttonText: buttons[index],
                       color: isOperator(buttons[index])
                           ? Colors.purple
@@ -87,6 +146,18 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    // finalQuestion= finalQuestion.replaceAll('from', replace)
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    userAnswer = eval.toString();
   }
 }
 
